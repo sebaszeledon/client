25a817a55ffdb11aa1e067f09b8f7641605958a3
sebaszeledon <sebas.zeledon@gmail.com>
2024-07-06 14:08:23 -0600

    update

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,10 @@
 import { createThirdwebClient } from "thirdweb";
 
+
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
+const clientId = import.meta.env.CLIENT_ID;
 
 export const client = createThirdwebClient({
-  clientId: clientId,
+  clientId: "rAQUnPq7vMRZbJchVSy6fpWB5IQSXR0frLY2Pj5a-BTQ1Dl-e1VmIhv1Gilw6gzPjhw5zIK9kv5QkG5R5pmJJQ",
 });
