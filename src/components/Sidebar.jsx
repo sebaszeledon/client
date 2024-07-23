b91ab455a6ddc7abb151b3c6a046e172c6d67435
sebaszeledon <sebas.zeledon@gmail.com>
2024-07-22 22:01:36 -0600

    update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,11 +4,23 @@ import { Link, useNavigate } from 'react-router-dom';
 import { logo, sun } from '../assets';
 import { navLinks } from '../constants';
 
+const Icon = () => (
+  <div>
+    
+  </div>
+);
+
 const Sidebar = () => {
+
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
+
   return (
-    <div>Sidebar</div>
+    <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
+      <Link to="/">
+        <Icon></Icon>
+      </Link>
+    </div>
   )
 }
 
