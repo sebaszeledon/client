01b9b9f1694c9db4690a14cc272f19fa8874a874
sebaszeledon <sebas.zeledon@gmail.com>
2024-07-24 22:53:23 -0600

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,11 +35,20 @@ const Navbar = () => {
         />
 
         <Link to="/profile">
-          <div>
-            <img src={thirdweb} />
+          <div className='w-[60px] h-[60px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
         </Link>
       </div>
+
+      {/*Small screen navigation*/}
+
+        <div className='sm:hidden flex justify-between items-center relative'> 
+          <div className='w-[52px] h-[52px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
+          </div>
+        </div>
+
     </div>
   )
 }
