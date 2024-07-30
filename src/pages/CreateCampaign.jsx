70d495052eb19eae7ffb9fb27066182a3181aa00
sebaszeledon <sebas.zeledon@gmail.com>
2024-07-29 21:58:31 -0600

    update

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { money } from '../assets';
+import { CustomButton } from '../components';
+import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
+  const navigate = useNavigate();
+  
   return (
     <div>CreateCampaign</div>
-  )
+  );
 }
 
 export default CreateCampaign
