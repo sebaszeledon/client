ed501ee53041e5d44106564b16891bb37ca03f6b
sebaszeledon <sebas.zeledon@gmail.com>
2024-08-21 21:59:39 -0600

    DisplayCampaigns updated

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const DisplayCampaigns = () => {
+import { loader } from '../assets';
+import FundCard from './FundCard';
+
+const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (campaigns) => {
+    navigate(`/campaign-details/${campaign.title}`, { state: campaign });
+  }
+
   return (
-    <div>DisplayCampaigns</div>
+    <div>
+      <hi className="font-epilogue font-semibold text-[18px] 
+      text-white text-left">{title} ({campaigns.length})</hi>
+      <div className="flex flex-wrap mt-[20px] gap-[6px]">
+        {isLoading && (
+            <img src={loader} alt='loader' className='w-[100px] h-[100px] object-contain'/>
+        )}
+        {!isLoading && campaigns.length === 0 && (
+            <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+                You have not created a campaign yet.
+            </p>
+        )}
+        {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => 
+        <FundCard
+            key={campaign.id}
+            {...campaign}
+            handleClick = {() => handleNavigate(campaign)}
+        />)}
+      </div>
+    </div>
   );
-}
+};
 
 export default DisplayCampaigns;
