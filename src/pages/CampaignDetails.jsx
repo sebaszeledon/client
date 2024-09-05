49069fa39b5bce1786a226bbdb2072eb6bbddc58
sebaszeledon <sebas.zeledon@gmail.com>
2024-09-04 21:14:17 -0600

    campaign details updated

--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -9,9 +9,31 @@ import { thirdweb } from '../assets';
 
 const CampaignDetails = () => {
   const { state } = useLocation();
-  console.log(state);
+  const { getDonations, contract, address } = useStateContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
+
   return (
-    <div>CampaignDetails</div>
+    <div>
+      {isLoading && (
+            <img src={loader} alt='loader' className='w-[100px] h-[100px] object-contain'/>
+      )}
+      <div className='w-full flex md:flex-row flex-col mt-10 gap-[30px]'>
+        <div className='flex-1 flex-col'>
+          <img src={state.image} alt='campaign' className='w-full h-[310px] object-cover rounded-xl'></img>
+          <div className='relative w-full h-[5px] bg-[#3a3a43] mt-2 rounded-xl'>
+            <div className='absolute h-full bg-[#4acd8d] rounded-xl'
+            style = {{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth:'100%'}}>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
