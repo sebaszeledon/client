9e5cb23bf8c66bb7f68840674039a8514cf50289
sebaszeledon <sebas.zeledon@gmail.com>
2024-09-09 21:02:04 -0600

    Campaign Details almost finished

--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -16,6 +16,10 @@ const CampaignDetails = () => {
 
   const remainingDays = daysLeft(state.deadline);
 
+  const handleDonate = async () => {
+
+  }
+
   return (
     <div>
       {isLoading && (
@@ -83,10 +87,43 @@ const CampaignDetails = () => {
                     No donators yet. Be the first one!
                   </p>
                 )}
-                
               </div>
           </div>
+        </div>
+        <div className="flex-1">
+          <h4 className='font-epilogue font-semibold text-[18px] text-white uppercase'>Fund</h4>
+          <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[12px]">
+            <p className="font-epilogue font-medium text-[20px] leading-[30px] text-center text-[#808191]">
+              Fund the campaign
+            </p>
+            <div className="mt-[20px]">
+              <input
+                type="number"
+                placeholder="ETH 0.1"
+                step="0.01"
+                className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none 
+                border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white
+                text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[12px]"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+              <div className="mt-[20px] p-4 bg-[#13131a] rounded-[12px]">
+                <h4 className="font-epilogue text-white font-semibold text-[14px] leading-[22px]">
+                  Back it because you believe in it.
+                </h4>
+                <p className="font-epilogue mt-[20px] font-normal leading-[22px] text-[#808191]">
+                  Support the project for no reward, just because it speaks to you.
+                </p>
+              </div>
 
+              <CustomButton
+                btnType="button"
+                title="Fund Campaign"
+                styles="w-full bg-[#8c6dfd] mt-[20px]"
+                handleClick={handleDonate}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -95,4 +132,4 @@ const CampaignDetails = () => {
 
 export default CampaignDetails;
 
-//2:50:40
+//3:10:07
